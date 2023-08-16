41bce040af4efd8f691e3bdbde86fede9cc5a4e9
chaneesong <sch102403@gmail.com>
2023-08-17 08:00:55 +0900

    test

--- .github/workflows/inject-id/index.js
+++ .github/workflows/inject-id/index.js
@@ -1,6 +1,7 @@
 const main = () => {
   const responseText = process.argv[2];
-  console.log(responseText.slice(4, responseText.length - 1));
+  const match = responseText.match(/\[add\](.*)/s);
+  console.log(match);
 };
 
 main();
