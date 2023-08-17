c6fa6fea5252d032cda8c6600ac3d361a146ac50
chaneesong <sch102403@gmail.com>
2023-08-18 06:40:13 +0900

    feat: 응답 객체 추출 후 마크다운으로 변환

--- .github/workflows/send-request/markdown/injectId/index.js
+++ .github/workflows/send-request/markdown/injectId/index.js
@@ -1,6 +1,42 @@
+const selectProperties = (response) => {
+  const {
+    id,
+    title,
+    content,
+    category: categoryData,
+    tags: tagsData,
+  } = response;
+  const { category } = categoryData;
+  const tags = tagsData.map((tagData) => tagData.keyword);
+  return { id, title, category, tags, content };
+};
+
+const parseHeader = (header) => {
+  let result = '';
+  const entiry = Object.entries(header);
+  for (const value of entiry) {
+    result = `${result}${value[0]}: ${value[1]}\n`;
+  }
+  return result.trim();
+};
+
+const convertToMarkdown = (object) => {
+  const { content, ...header } = object;
+  const headerText = parseHeader(header);
+  const markdownText = `---
+${headerText}
+---
+
+${object.content}`;
+
+  return markdownText;
+};
+
 export const injectId = (data) => {
   try {
-    console.log(data);
+    const parsedResponse = selectProperties(data);
+    const markdown = convertToMarkdown(parsedResponse);
+    console.log(markdown);
   } catch (error) {
     console.error(error);
     process.exit(0);
