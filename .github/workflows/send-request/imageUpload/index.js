1ce12cd5fb5c9cd8627ffa3e21f9775408d1a0da
chaneesong <sch102403@gmail.com>
2023-10-30 20:15:07 +0900

    feat: oauth2 인증 요소 파라이터로 받도록 수정

--- .github/workflows/send-request/imageUpload/index.js
+++ .github/workflows/send-request/imageUpload/index.js
@@ -1,10 +1,10 @@
 import { google } from 'googleapis';
 
-const getOAuth2Client = () => {
+const getOAuth2Client = (clientID, clientSecret, redirectURL) => {
   const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URL
+    clientID,
+    clientSecret,
+    redirectURL
   );
 
   oauth2Client.setCredentials({
@@ -14,4 +14,12 @@ const getOAuth2Client = () => {
   return oauth2Client;
 };
 
-export const uploadImage = async () => {};
+export const uploadImage = async () => {
+  const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL } =
+    process.env;
+  const oauth2Client = getOAuth2Client(
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
+    GOOGLE_REDIRECT_URL
+  );
+};
