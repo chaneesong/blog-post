85541ff902a7eaae014e50f3d56b29cff574cbcb
chaneesong <sch102403@gmail.com>
2023-10-31 08:34:12 +0900

    feat: 구글 드라이브에 이미지 업로드 기능 구현

--- .github/workflows/send-request/imageUpload/index.js
+++ .github/workflows/send-request/imageUpload/index.js
@@ -1,4 +1,6 @@
 import { google } from 'googleapis';
+import { createReadStream, readdirSync } from 'node:fs';
+import { __dirname } from '../utils/getDirName';
 
 const getOAuth2Client = (clientID, clientSecret, redirectURL) => {
   const oauth2Client = new google.auth.OAuth2(
@@ -33,6 +35,42 @@ const createFolder = async (drive, imagePath) => {
   return id;
 };
 
+const getImageName = (imagePath) => {
+  return readdirSync(`${__dirname}/${imagePath}`);
+};
+
+const parseImageName = (ImgName) => {
+  const nameArr = ImgName.split('.');
+  return {
+    name: nameArr[0],
+    ext: nameArr[1],
+  };
+};
+
+const uploadImageToDrive = async (drive, imagePath, folderId) => {
+  const responsePromises = [];
+
+  const imgNames = getImageName(imagePath);
+
+  for (let imgName of imgNames) {
+    const { ext } = parseImageName(imgName);
+    const responsePromise = drive.files.create({
+      requestBody: {
+        name: imgName,
+        parents: [folderId],
+        MimeType: `image/${ext}`,
+      },
+      media: {
+        MimeType: `image/${ext}`,
+        body: createReadStream(`${imagePath}/${imgName}`),
+      },
+    });
+    responsePromises.push(responsePromise);
+  }
+
+  return (await Promise.all(responsePromises)).map((res) => res.data.id);
+};
+
 export const uploadImage = async (imagePath) => {
   const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL } =
     process.env;
@@ -44,6 +82,8 @@ export const uploadImage = async (imagePath) => {
     );
     const drive = getDriveAccess(oauth2Client);
     const folderId = createFolder(drive, imagePath);
+    const imgIds = await uploadImageToDrive(drive, imagePath, folderId);
+    return imgIds;
   } catch (error) {
     console.error('이미지를 업로드 하는 도중 에러 발생!');
     console.error(error);
