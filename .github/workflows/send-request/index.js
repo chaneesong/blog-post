c69a0872c5aae63c2cff97a229e37ae6f9b1f8bf
chaneesong <sch102403@gmail.com>
2023-11-10 17:38:55 +0900

    feat: 이미지 업로드 로직 연걸

--- .github/workflows/send-request/index.js
+++ .github/workflows/send-request/index.js
@@ -3,6 +3,7 @@ import checkHeaderValidity from './markdown/headerValidation/index.js';
 import sendRequestByFileType from './markdown/sendRequestByFileType.js';
 import { filterMarkdownToPushedFiles } from './parser/parsePushedFile.js';
 import { getMarkdownContents } from './markdown/getMarkdown.js';
+import { changeImageUrl } from './markdown/changeImageUrl.js';
 
 const main = async () => {
   let hasValidationFailed = false;
@@ -12,10 +13,14 @@ const main = async () => {
     const pushedFiles = filterMarkdownToPushedFiles(pushedFileText);
 
     for (const [fileType, filePath] of pushedFiles) {
+      const imgIds = uploadImage(filePath);
       const { stdout } = await getMarkdownContents(fileType, filePath);
-      const markdown = frontMatter(stdout);
-
+      const modifiedMarkdownContent = changeImageUrl(stdout, imgIds);
+      const markdown = frontMatter(modifiedMarkdownContent);
       const { attributes, body } = markdown;
+      console.log('attr', attributes);
+      console.log('body', body);
+
       checkHeaderValidity(fileType, attributes);
       sendRequestByFileType(fileType, attributes, body);
     }
