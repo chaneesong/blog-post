04c072a61cfccca54b649b43a6fa9e54bc68ace0
chaneesong <sch102403@gmail.com>
2023-11-13 15:08:03 +0900

    test cwd

--- .github/workflows/send-request/utils/getDirname.js
+++ .github/workflows/send-request/utils/getDirname.js
@@ -1,6 +1,5 @@
 // __dirname을 사용하기 위해 정의
-import { dirname } from 'node:path';
+import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-const __filename = fileURLToPath(import.meta.url);
-export const __dirname = dirname(__filename);
+export const __dirname = path.dirname(process.cwd());
