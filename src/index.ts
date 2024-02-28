6516aa0f1b3c0aa67b18236d567f87a69c1f7d64
chaneesong <sch102403@gmail.com>
2024-02-29 05:47:30 +0900

    feat: webhook 서버 구현

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { writeFileSync } from 'node:fs';
+import axios from 'axios';
 
 const app = express();
 const port = 8080;
@@ -7,11 +8,28 @@ const port = 8080;
 app.use(express.json());
 
 app.get('/', (req, res) => {
+  writeFileSync('output.log', 'ok');
   return res.sendStatus(200);
 });
 
-app.post('/', (req, res) => {
-  writeFileSync('output.log', req.body.test);
+app.post('/auth/login', async (req, res) => {
+  const { id, password } = req.body;
+
+  const result = await axios.post('http://localhost/api/auth/login', {
+    id,
+    password,
+  });
+  return res.status(200).send(result.data);
+});
+
+app.post('/posts', async (req, res) => {
+  const config = { headers: { Authorization: req.headers.authorization } };
+  const result = await axios.post(
+    'http://localhost/api/posts',
+    req.body,
+    config
+  );
+  console.log(result.data);
   return res.sendStatus(200);
 });
 
